4efabac2562a5559c53188fcf628d1c3bc3cd609
DaanvanHerwaarden <daan.vanherwaarden@hotmail.com>
2017-05-08 11:24:03 +0200

    commit procfile

---
+++ server.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Daan on 4-5-2017.
+ */
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function(request, response) {
+    response.send('Hello Avans!');
+})
+
+app.get('/about', function(request, response) {
+    response.send('Written by <jouw naam hier invullen>');
+})
+
+app.post('/', function(request, response) {
+    response.send('Hello Avans, POST request received!');
+})
+
+app.put('/', function(request, response) {
+    response.send('Hello Avans, PUT request received!');
+})
+
+app.all('*', function(request, response) {
+    response.status(404);
+    response.send('404 - Not found');
+})
+
+app.listen(port, function() {
+    console.log('Server app is listening on port ' + port);
+})
+
+
